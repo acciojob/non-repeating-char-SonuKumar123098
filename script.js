a726859a0b83f8db09db0643672792e77f55090d
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-26 15:41:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const map={};
+	for(let i=0;i<str.length;i++){
+		map[str[i]]= (map[str[i]]||0) +1;
+	}
+	for(const char of str){
+		if(map[char]===1) return char;
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
